b386d8e3fbc7eca372b4e937326e232e6dabcafe
Marco <marcoaremail@gmail.com>
2016-05-03 12:11:28 -0300

    Set color attribute

--- background.h
+++ background.h
@@ -19,9 +19,15 @@ class BackgroundColor : virtual public Background {
     Color color;
 
 public:
-    inline BackgroundColor (const Color &_color) : color(_color) {};
+    inline BackgroundColor () : color(Color::rgb(0, 0, 0)) {};
+    inline BackgroundColor (Color _color) : color(_color) {};
 
-    inline void setColor (const Color &_color) { this->color = _color; }
+    inline void setColor (Color &_color) { this->color = _color; }
+
+    inline void setR (unsigned char _r) { this->color.setR(_r); }
+    inline void setG (unsigned char _g) { this->color.setR(_g); }
+    inline void setB (unsigned char _b) { this->color.setR(_b); }
+    inline void setA (double _a) { this->color.setA(_a); }
 
     inline void apply (void) const { this->color.apply(); }
 };
--- color.h
+++ color.h
@@ -1,6 +1,7 @@
 #ifndef SRC_ENGINE_COLOR_H_
 #define SRC_ENGINE_COLOR_H_
 
+#include <string>
 #include <GLFW/glfw3.h>
 
 class Color {
@@ -19,6 +20,26 @@ public:
     inline static Color rgb (unsigned char _r, unsigned char _g, unsigned char _b) {
         return Color(_r / 255.0, _g / 255.0, _b / 255.0, 1.0);
     }
+
+    inline static Color rgb (unsigned value) {
+        return rgb((value >> 16) & 255, (value >> 8) & 255, value & 255);
+    }
+
+    inline static Color rgba (unsigned value) {
+        return rgba((value >> 16) & 255, (value >> 8) & 255, value & 255, static_cast<double>((value >> 24) & 255) / 255.0);
+    }
+
+    inline static Color hex (std::string str) {
+        if (str[0] == '#') {
+            str = str.substr(1);
+        }
+        return rgb(stoul(str, nullptr, 16));
+    }
+
+    inline void setR (unsigned char _r) { this->r = _r; }
+    inline void setG (unsigned char _g) { this->g = _g; }
+    inline void setB (unsigned char _b) { this->b = _b; }
+    inline void setA (double _a) { this->a = _a; }
 
     inline void apply (void) const { glColor4d(this->r, this->g, this->b, this->a); }
 
