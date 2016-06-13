9d5e4a4b3958caaa4cb6a6c4d1b5b19e639986a4
Marco <marcoaremail@gmail.com>
2016-06-13 06:57:50 -0300

    Draw using triangles and own matrixes

--- draw.cc
+++ draw.cc
@@ -4,4 +4,13 @@ namespace Engine {
 
     unsigned Draw::drawn = 0;
 
+    std::stack<std::array<float_max_t, 16>> Draw::matrix_stack;
+    Background *Draw::background;
+    std::array<float_max_t, 16> Draw::matrix = {
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    };
+
 };
--- draw.h
+++ draw.h
@@ -3,9 +3,13 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <xmmintrin.h>
 #include "defaults.h"
 #include "vec.h"
 #include "quaternion.h"
+#include "background.h"
+#include "shader.h"
 
 namespace Engine {
 
@@ -13,58 +17,130 @@ namespace Engine {
 
     static unsigned drawn;
 
+    static std::stack<std::array<float_max_t, 16>> matrix_stack;
+    static std::array<float_max_t, 16> matrix;
+    static Background *background;
+
+    static void multiplyMatrix (const std::array<float_max_t, 16> &to) {
+
+        // std::array<float_max_t, 16> result;
+        // const float_max_t *A = &to[0], *B = &matrix[0];
+        // float_max_t *C = &result[0];
+        // __m256d
+        //     col1 = _mm256_load_pd(&B[ 0]),
+        //     col2 = _mm256_load_pd(&B[ 4]),
+        //     col3 = _mm256_load_pd(&B[ 8]),
+        //     col4 = _mm256_load_pd(&B[12]);
+        // for(unsigned i = 0; i < 4; ++i) {
+        //     unsigned row = i << 2;
+        //     _mm256_store_pd(&C[row], _mm256_add_pd(
+        //             _mm256_add_pd(
+        //                 _mm256_mul_pd(_mm256_set1_pd(A[row]), col1),
+        //                 _mm256_mul_pd(_mm256_set1_pd(A[row + 1]), col2)
+        //             ),
+        //             _mm256_add_pd(
+        //                 _mm256_mul_pd(_mm256_set1_pd(A[row + 2]), col3),
+        //                 _mm256_mul_pd(_mm256_set1_pd(A[row + 3]), col4)
+        //             )
+        //         )
+        //     );
+        // }
+        // matrix.swap(result);
+
+        // matrix = {
+        //     matrix[ 0] * to[ 0] + matrix[ 1] * to[ 4] + matrix[ 2] * to[ 8] + matrix[ 3] * to[12],
+        //     matrix[ 0] * to[ 1] + matrix[ 1] * to[ 5] + matrix[ 2] * to[ 9] + matrix[ 3] * to[13],
+        //     matrix[ 0] * to[ 2] + matrix[ 1] * to[ 6] + matrix[ 2] * to[10] + matrix[ 3] * to[14],
+        //     matrix[ 0] * to[ 3] + matrix[ 1] * to[ 7] + matrix[ 2] * to[11] + matrix[ 3] * to[15],
+        //
+        //     matrix[ 4] * to[ 0] + matrix[ 5] * to[ 4] + matrix[ 6] * to[ 8] + matrix[ 7] * to[12],
+        //     matrix[ 4] * to[ 1] + matrix[ 5] * to[ 5] + matrix[ 6] * to[ 9] + matrix[ 7] * to[13],
+        //     matrix[ 4] * to[ 2] + matrix[ 5] * to[ 6] + matrix[ 6] * to[10] + matrix[ 7] * to[14],
+        //     matrix[ 4] * to[ 3] + matrix[ 5] * to[ 7] + matrix[ 6] * to[11] + matrix[ 7] * to[15],
+        //
+        //     matrix[ 8] * to[ 0] + matrix[ 9] * to[ 4] + matrix[10] * to[ 8] + matrix[11] * to[12],
+        //     matrix[ 8] * to[ 1] + matrix[ 9] * to[ 5] + matrix[10] * to[ 9] + matrix[11] * to[13],
+        //     matrix[ 8] * to[ 2] + matrix[ 9] * to[ 6] + matrix[10] * to[10] + matrix[11] * to[14],
+        //     matrix[ 8] * to[ 3] + matrix[ 9] * to[ 7] + matrix[10] * to[11] + matrix[11] * to[15],
+        //
+        //     matrix[12] * to[ 0] + matrix[13] * to[ 4] + matrix[14] * to[ 8] + matrix[15] * to[12],
+        //     matrix[12] * to[ 1] + matrix[13] * to[ 5] + matrix[14] * to[ 9] + matrix[15] * to[13],
+        //     matrix[12] * to[ 2] + matrix[13] * to[ 6] + matrix[14] * to[10] + matrix[15] * to[14],
+        //     matrix[12] * to[ 3] + matrix[13] * to[ 7] + matrix[14] * to[11] + matrix[15] * to[15]
+        // };
+    }
+
+    static inline void transformVertex (Vec<3> &vertex) {
+        // vertex[0] = (vertex[0] * matrix[0]) + (vertex[1] * matrix[4]) + (vertex[1] * matrix[ 8]) + matrix[12];
+        // vertex[1] = (vertex[0] * matrix[1]) + (vertex[1] * matrix[5]) + (vertex[1] * matrix[ 9]) + matrix[13];
+        // vertex[2] = (vertex[0] * matrix[2]) + (vertex[1] * matrix[6]) + (vertex[1] * matrix[10]) + matrix[14];
+    }
+
     public:
 
-        inline static void begin (GLuint type) {
+        inline static void begin (Background *_background = nullptr) {
+            // TODO lock
+            background = _background;
+            glBegin(GL_TRIANGLES);
             drawn = 0;
-            glBegin(type);
         }
 
         inline static unsigned end (void) {
+            unsigned total = drawn;
             glEnd();
-            return drawn;
+            background = nullptr;
+            // TODO unlock
+            return total;
         }
 
 // -----------------------------------------------------------------------------
 
-        inline static void vertex (const Vec<3> &vert) {
+        inline static void vertex (Vec<3> vert) {
+            if (background) {
+                background->apply();
+            }
             ++drawn;
-            glVertex3d(vert[0], vert[1], vert[2]);
+            // transformVertex(vert);
+            glVertex3dv(vert.data());
         }
 
         inline static void vertex (float_max_t vert_0, float_max_t vert_1, float_max_t vert_2) {
-            ++drawn;
-            glVertex3d(vert_0, vert_1, vert_2);
+            vertex({ vert_0, vert_1, vert_2 });
         }
 
 // -------------------------------------
 
-        inline static void normal (const Vec<3> &vec) {
-            glNormal3d(vec[0], vec[1], vec[2]);
+        inline static void normal (Vec<3> vec) {
+            glNormal3dv(vec.data());
         }
 
         inline static void normal (float_max_t vert_0, float_max_t vert_1, float_max_t vert_2) {
-            glNormal3d(vert_0, vert_1, vert_2);
+            normal({ vert_0, vert_1, vert_2 });
         }
 
 // -----------------------------------------------------------------------------
 
-        inline static void translate (const Vec<3> &vec) {
-            if (vec) {
-                glTranslated(vec[0], vec[1], vec[2]);
+        inline static void translate (float_max_t vert_0, float_max_t vert_1, float_max_t vert_2) {
+            if (vert_0 || vert_1 || vert_2) {
+                multiplyMatrix({
+                       1.0,    0.0,   0.0,  0.0,
+                       0.0,    1.0,   0.0,  0.0,
+                       0.0,    0.0,   1.0,  0.0,
+                    vert_0, vert_1, vert_2, 1.0
+                });
+                glTranslated(vert_0, vert_1, vert_2);
             }
         }
 
-        inline static void translate (float_max_t vert_0, float_max_t vert_1, float_max_t vert_2) {
-            if (vert_0 || vert_1 || vert_2) {
-                glTranslated(vert_0, vert_1, vert_2);
-            }
+        inline static void translate (const Vec<3> &vec) {
+            translate(vec[0], vec[1], vec[2]);
         }
 
 // -------------------------------------
 
         inline static void rotate (const Quaternion &quat) {
             if (!quat.isIdentity()) {
+                multiplyMatrix(quat.rotation());
                 glMultMatrixd(quat.rotation().data());
             }
         }
@@ -72,10 +148,13 @@ namespace Engine {
 // -----------------------------------------------------------------------------
 
         inline static void push (void) {
+            matrix_stack.push(matrix);
             glPushMatrix();
         }
 
         inline static void pop (void) {
+            matrix = matrix_stack.top();
+            matrix_stack.pop();
             glPopMatrix();
         }
 
